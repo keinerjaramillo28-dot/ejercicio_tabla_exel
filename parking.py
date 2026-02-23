7399647a9689918ab2e66fbb09275a0d4cfee362
keinerjaramillo28-dot <keinerjaramillo28@gmail.com>
2026-02-23 08:34:41 -0500

    Add text table printing to parqueadero class

--- parking.py
+++ parking.py
@@ -6,6 +6,7 @@ class parqueadero:
         self.hora_entrada = ""
         self.hora_salida = ""
         self.estado = ""
+        self.textabla = ""
 
     def registrar_entrada(self, fecha, hora):
         self.fecha_entrada = input("Ingrese la fecha de entrada (DD/MM/AAAA): ")
@@ -17,8 +18,11 @@ class parqueadero:
         self.hora_salida = input("Ingrese la hora de salida (HH:MM): ")
         self.estado = "FUERA"
         print("salida registrada exitosamente.")
-    def mostrar_informacion(self):
-        print(f"Fecha de entrada: {self.fecha_entrada}")
-        print(f"Hora de entrada: {self.hora_entrada}")
-        print(f"Hora de salida: {self.hora_salida}")
-        print(f"Estado: {self.estado}")
+     def imprimir_informacion(self):
+        self.textabla = self.textabla + f"{'Cedula':<12} | {'Nombre':<12} | {'Rango':<10} | {'Marca':<10} | {'Placa':<12} | {'Color':<10} | {'Fecha Entrada':<12} | {'Hora Entrada':<10} | {'Hora Salida':<10} | {'Estado':<10} \n"
+        self.textabla = self.textabla + "-"*130 + "\n"
+        self.textabla += f"{self.usuario.cedula:<12} | {self.usuario.nombre:<12} | {self.usuario.rango:<10} | {self.carro.placa:<10} | {self.carro.marca:<12} | {self.carro.color:<10} | {self.fecha_entrada:<12} | {self.hora_entrada:<10} | {self.hora_salida:<10} | {self.estado:<10}\n"
+
+        print(self.textabla)
+
+
